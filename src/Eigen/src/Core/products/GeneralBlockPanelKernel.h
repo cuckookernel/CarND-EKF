bbf8fbb446eff1a402fcbeb7c9d2dd8a7301daf4
Mateo Restrepo <mateo.restrepo@yuxiglobal.com>
2019-02-27 10:25:53 -0500

    minor

--- src/Eigen/src/Core/products/GeneralBlockPanelKernel.h
+++ src/Eigen/src/Core/products/GeneralBlockPanelKernel.h
@@ -571,7 +571,7 @@ void gebp_kernel<LhsScalar,RhsScalar,Index,mr,nr,ConjugateLhs,ConjugateRhs>
                   traits.initAcc(C0);
                   traits.initAcc(C1);
         if(nr==4) traits.initAcc(C2);
-        if(nr==4) traits.initAcc(C3);
+        if(nr==4){ traits.initAcc(C3); }
                   traits.initAcc(C4);
                   traits.initAcc(C5);
         if(nr==4) traits.initAcc(C6);
@@ -943,12 +943,12 @@ EIGEN_ASM_COMMENT("mybegin4");
                   R0 = ploadu<ResPacket>(r0);
                   R1 = ploadu<ResPacket>(r1);
         if(nr==4) R2 = ploadu<ResPacket>(r2);
-        if(nr==4) R3 = ploadu<ResPacket>(r3);
+        if(nr==4) { R3 = ploadu<ResPacket>(r3); }
 
                   traits.acc(C0, alphav, R0);
                   traits.acc(C1, alphav, R1);
         if(nr==4) traits.acc(C2, alphav, R2);
-        if(nr==4) traits.acc(C3, alphav, R3);
+        if(nr==4) { traits.acc(C3, alphav, R3); }
 
                   pstoreu(r0, R0);
                   pstoreu(r1, R1);
